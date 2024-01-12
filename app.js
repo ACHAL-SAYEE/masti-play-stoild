2cc4e23545ffdb3cd6a23734c098a38e292da51d
Shivanshu Gupta <shivanshukgupta@gmail.com>
2024-01-12 05:37:35 +0530

    New Update

--- app.js
+++ app.js
@@ -635,11 +635,11 @@ async function startANewGame() {
   try {
     setTimeout(gameStarts, 0, io); // Betting Starts
     setTimeout(bettingEnds, 30000); // Betting Ends & send result
-    setTimeout(gameEnds, 40000, io); // 10 sec spinner + 10 sec leaderboard
+    setTimeout(gameEnds, 42000, io); // 10 sec spinner + 10 sec leaderboard
   } catch (e) {
     console.error("Error in Game:", e);
   }
-  setTimeout(startANewGame, 40000); // New Game Begins
+  setTimeout(startANewGame, 45000); // New Game Begins
 }
 
 startANewGame();
