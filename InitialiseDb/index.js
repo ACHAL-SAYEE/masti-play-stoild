90e34039dbdf69859ca6cc60005761e9692eb61f
achal <achalsayee2@gmail.com>
2024-01-21 19:29:38 +0530

    New Update

--- InitialiseDb/index.js
+++ InitialiseDb/index.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 
 const initializeDB = async () => {
   try {
-    // await mongoose.connect('mongodb+srv://achalsayee2:FAWltJHPQOWRFggR@otbound.c8kfayb.mongodb.net/?retryWrites=true&w=majority', {
-    await mongoose
-      .connect("mongodb://0.0.0.0:27017/mastiplay", {
+    await mongoose.connect('mongodb+srv://achalsayee2:FAWltJHPQOWRFggR@otbound.c8kfayb.mongodb.net/?retryWrites=true&w=majority', {
+    // await mongoose
+    //   .connect("mongodb://0.0.0.0:27017/mastiplay", {
         useNewUrlParser: true,
         useUnifiedTopology: true,
       })
