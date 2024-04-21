280c37ed16de13f9f9bdcce4a5c090314c26a0c1
Rohan goyal <55226682+rohangoyalrg@users.noreply.github.com>
2024-04-21 16:42:27 +0530

    Update index.js

--- InitialiseDb/index.js
+++ InitialiseDb/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const initializeDB = async () => {
   try {
     // await mongoose.connect('mongodb+srv://achalsayee2:FAWltJHPQOWRFggR@otbound.c8kfayb.mongodb.net/?retryWrites=true&w=majority', {
-    await mongoose.connect("mongodb://0.0.0.0:27017/mastiplay", {
+    await mongoose.connect("mongodb://0.0.0.0:27017/mastiplay1", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
